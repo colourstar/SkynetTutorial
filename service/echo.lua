0b3cadcdfdb1b86f6448958d9729c3943e911151
colourstar <colourstar27@qq.com>
2018-05-16 17:11:28 +0800

    first service test

---
+++ service/echo.lua
@@ -0,0 +1,25 @@
+local skynet = require "skynet"
+require 'skynet.manager'
+
+local command = {}
+
+function command.HELLO(what)
+    return 'i am echo server and get this:' .. what
+end
+
+function dispatcher()
+    skynet.dispatch('lua',
+        function( session, address, cmd, ...)
+            cmd = cmd:uppder()
+            if (cmd == "HELLO") then
+                local f = command[cmd]
+                assert(f)
+                skynet.ret(skynet.pack(f(...)))
+            end
+        end
+    )
+
+    skynet.register('echo')
+end
+
+skynet.start(dispatcher)
